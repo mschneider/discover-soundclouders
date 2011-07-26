ad6c304699c639fcd04bc679f104ed2f111106cc
M.Schneider <not.m.schneider@gmail.com>
2011-07-27 00:35:39 +0200

    initial oauth try

--- proxy.rb
+++ proxy.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
+require 'soundcloud'
 
-get '/' do
-  redirect '/index.html'
+get '/login' do
+  client = Soundcloud.new({:client_id => '6c574a63595f5f55c82cd58f945f932a'})
+  redirect client.authorize_url(:redirect_uri => 'http://soundbone.herokuapp.com/oauth')
+end
+
+get '/oauth' do
+  params[:code] + '|' + params[:access_token] + '|' + params[:expires_in]
 end
