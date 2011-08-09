28634cef2be413a47632ce6a473db8c7765f57a6
M.Schneider <not.m.schneider@gmail.com>
2011-08-09 19:34:37 +0200

    added retry in ajax call - heroku kills everything that takes longer then 30 seconds

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,25 @@
 $(function(){
   Recommendations = new ApplicationController();
-  $.get('/me/recommendations', function(recommendations) {
-    Recommendations.set({ recommendedUsers: new UserList(JSON.parse(recommendations)) });
-    Recommendations.display(0);
-    $('body').empty();
-    _(Views).each(function(view) {
-      $('body').append(view.el);
-    });
+  $.ajax({
+    url: '/me/recommendations',
+    tryCount: 0,
+    retryLimit: 60,
+    success: function(recommendations) {
+      Recommendations.set({ recommendedUsers: new UserList(JSON.parse(recommendations)) });
+      Recommendations.display(0);
+      $('body').empty();
+      _(Views).each(function(view) {
+        $('body').append(view.el);
+      });
+    },
+    error: function() {
+      if (this.tryCount <= this.retryLimit) {
+        this.tryCount++;
+        $.ajax(this);
+      } else {
+        $('body p').html('Something seems to be wrong. Please try again later.');
+      }
+    }
   });
   
   Player = new PlayerController();
